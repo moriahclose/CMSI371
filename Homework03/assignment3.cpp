1f190ffc62c186d40d9bcb37f32bf2f812da5fc8
mtollive <mtollive@lion.lmu.edu>
2019-04-12 11:00:52 -0700

    Added chair

--- Homework03/assignment3.cpp
+++ Homework03/assignment3.cpp
@@ -278,8 +278,42 @@ vector<GLfloat> init_scene() {
     vector<GLfloat> desk_top = mat_mult(scaling_matrix(1.6, 0.08, 1.2), build_cube());
     desk_top = mat_mult(translation_matrix(-1.0, -0.4, -1.4), desk_top);
 
+    //monitor
+    vector<GLfloat> monitor = mat_mult(scaling_matrix(0.8, 0.6, 0.02), build_cube());
+    monitor = mat_mult(rotation_matrix_x(deg2rad(-10)), monitor);
+    monitor = mat_mult(translation_matrix(-1.2, 0.3, -1.4), monitor);
+
+    vector<GLfloat> stand = mat_mult(scaling_matrix(0.15, 0.4, 0.05), build_cube());
+    stand = mat_mult(translation_matrix(-1.2, 0.0, -1.4), stand);
+
+    vector<GLfloat> monitor_bottom = mat_mult(scaling_matrix(0.3, 0.04, 0.3), build_cube());
+    monitor_bottom = mat_mult(translation_matrix(-1.2, -0.2, -1.4), monitor_bottom);
+
+    //chair
+    vector<GLfloat> chair_back = mat_mult(scaling_matrix(0.6, 1.2, 0.09), build_cube());
+    chair_back = mat_mult(translation_matrix(-1.0, -0.6, 0.0), chair_back);
+
+    vector<GLfloat> chair_bottom = mat_mult(scaling_matrix(0.6, 0.09, 0.6), build_cube());
+    chair_bottom = mat_mult(translation_matrix(-1.0, -1.2, -0.3), chair_bottom);
+
+    vector<GLfloat> chair_stand = mat_mult(scaling_matrix(0.075, 1.0, 0.075), build_cube());
+    chair_stand = mat_mult(translation_matrix(-1.0, -1.68, -0.2), chair_stand); 
+
+    vector<GLfloat> chair_leg_back = mat_mult(scaling_matrix(0.075, 0.075, 0.6), build_cube());
+    chair_leg_back = mat_mult(translation_matrix(-1.0, -1.9, -0.4), chair_leg_back);
+
+    vector<GLfloat> chair_leg_right = mat_mult(scaling_matrix(0.6, 0.075, 0.075), build_cube());
+    chair_leg_right = mat_mult(translation_matrix(-0.8, -1.9, -0.2), chair_leg_right);
+
+    vector<GLfloat> chair_leg_front = mat_mult(scaling_matrix(0.075, 0.075, 0.6), build_cube());
+    chair_leg_front = mat_mult(translation_matrix(-1.0, -1.9, 0.0), chair_leg_front);
+
+    vector<GLfloat> chair_leg_left = mat_mult(scaling_matrix(0.6, 0.075, 0.075), build_cube());
+    chair_leg_left = mat_mult(translation_matrix(-1.2, -1.9, -0.2), chair_leg_left);
+
     scene = back_wall;
     scene.insert(scene.end(), left_wall.begin(), left_wall.end());
+    
     scene.insert(scene.end(), floor.begin(), floor.end());
 
     scene.insert(scene.end(), board_border.begin(), board_border.end());
@@ -288,6 +322,19 @@ vector<GLfloat> init_scene() {
     scene.insert(scene.end(), desk_leg_right.begin(), desk_leg_right.end());
     scene.insert(scene.end(), desk_leg_left.begin(), desk_leg_left.end());
     scene.insert(scene.end(), desk_top.begin(), desk_top.end());
+
+    scene.insert(scene.end(), monitor.begin(), monitor.end());
+    scene.insert(scene.end(), stand.begin(), stand.end());
+    scene.insert(scene.end(), monitor_bottom.begin(), monitor_bottom.end());
+
+    scene.insert(scene.end(), chair_back.begin(), chair_back.end());
+    scene.insert(scene.end(), chair_bottom.begin(), chair_bottom.end());
+    scene.insert(scene.end(), chair_stand.begin(), chair_stand.end());
+    scene.insert(scene.end(), chair_leg_back.begin(), chair_leg_back.end());
+    scene.insert(scene.end(), chair_leg_right.begin(), chair_leg_right.end());
+    scene.insert(scene.end(), chair_leg_left.begin(), chair_leg_left.end());
+    scene.insert(scene.end(), chair_leg_front.begin(), chair_leg_front.end());
+    //scene.insert(scene.end(), back_wheel.begin(), back_wheel.end());
     scene = to_cartesian_coord(scene);
     return scene;
 }
